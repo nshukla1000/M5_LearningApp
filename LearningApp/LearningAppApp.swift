bff7528c82c4e8ab25c26a5edcf33092d425588c
Nilesh Shukla <nileshshukla@gmail.com>
2021-09-13 23:05:58 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Tarjani Shukla on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
